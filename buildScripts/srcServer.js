6f5214abf72353c99d5d65886690f0a03507db9b
Iulian Oana <iulian.oana11@gmail.com>
2019-02-20 11:51:49 +0000

    Fixed #6. Downgraded babel-loader to match package babel-core.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
-
 import config from '../webpack.config.dev';
 
 const port = 3000;
