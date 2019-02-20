9b84dbf0a58e006a222e106e7472bcfd2010caae
Iulian Oana <iulian.oana11@gmail.com>
2019-02-20 15:08:38 +0000

    Added a test for dom using jsdom and fs and script for watch chai test.

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,28 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () =>{
   it('should pass', () => {
-    expect(true).to.equal(false);
+    expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  // Define a done arg for asynchrouness.
+  it('should say hello', (done) => {
+    // Get the html page into a constant.
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    // Pass the dom into the jsdom function.
+    jsdom.env(index, function (err, window){
+      // Get the first heading element rendered.
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      // Write the test
+      expect(h1.innerHTML).to.equal("Hello World!");
+      // When the page anynchrounously loaded
+      done();
+      // Finish the test
+      window.close();
+    });
   });
 });
