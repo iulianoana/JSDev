b8ccaeb4b553440cf1775eb2673d19545f08ca4f
Iulian Oana <iulian.oana11@gmail.com>
2019-02-20 12:17:49 +0000

    Using inline-source-map for in-browser debugging.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
+//debugger; // use debugger keyword to add a breakpoint.
 console.log(`I would pay ${courseValue} for this awesome course!`);
